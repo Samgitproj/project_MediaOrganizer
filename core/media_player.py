d673c3a6ea62f90660b163d0ee74fbea3914ac38
Samgitproj <sam.projects.vdl@gmail.com>
2025-08-14 19:43:43 +0200

    Normalize markers: media_player.py

--- core/media_player.py
+++ core/media_player.py
@@ -1,3 +1,4 @@
+# [SECTION: Imports]
 import os
 import logging
 from PyQt6.QtCore import QTimer
@@ -7,7 +8,10 @@ from PyQt6.QtMultimediaWidgets import QVideoWidget
 from PyQt6.QtWidgets import QLabel
 
 
+# [END: Imports]
+# [CLASS: MediaPlayer]
 class MediaPlayer:
+# [FUNC: __init__]
     def __init__(
         self, media_label: QLabel, video_widget: QVideoWidget, player: QMediaPlayer
     ):
@@ -22,6 +26,8 @@ class MediaPlayer:
         self.is_paused = False
         logging.debug("MediaPlayer: geïnitialiseerd")
 
+# [END: __init__]
+# [FUNC: play_media]
     def play_media(self, filepath: str):
         logging.debug(f"MediaPlayer: speelt af {filepath}")
 
@@ -39,6 +45,8 @@ class MediaPlayer:
         else:
             logging.warning(f"Ongeldig mediabestand: {filepath}")
 
+# [END: play_media]
+# [FUNC: play_next_media]
     def play_next_media(self):
         if not self.media_paths:
             logging.info("MediaPlayer: geen media te tonen")
@@ -50,6 +58,8 @@ class MediaPlayer:
         volgende_pad = self.media_paths[self.current_media_index]
         self.play_media(volgende_pad)
 
+# [END: play_next_media]
+# [FUNC: start_slideshow]
     def start_slideshow(self, interval_secs=5):
         if not self.media_paths:
             logging.info("Geen mediabestanden om slideshow te starten.")
@@ -61,6 +71,8 @@ class MediaPlayer:
         self.play_media(self.media_paths[self.current_media_index])
         logging.info("Slideshow gestart.")
 
+# [END: start_slideshow]
+# [FUNC: pause_slideshow]
     def pause_slideshow(self):
         if self.is_playing:
             if self.is_paused:
@@ -71,8 +83,12 @@ class MediaPlayer:
                 logging.info("Slideshow gepauzeerd.")
             self.is_paused = not self.is_paused
 
+# [END: pause_slideshow]
+# [FUNC: stop_slideshow]
     def stop_slideshow(self):
         self.slideshow_timer.stop()
         self.is_playing = False
         self.is_paused = False
         logging.info("Slideshow gestopt.")
+# [END: MediaPlayer]
+# [END: stop_slideshow]
